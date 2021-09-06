fbe3d065fcf0bb4e08d054ce59afefc8b5977669
derotto54 <jason.ganiatsas@gmail.com>
2021-09-05 20:29:31 -0400

    made the temp thing look nice

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 //Get users local temp
 function getMyTemp() {
 
-    // Set variable
-    let myTemp = ''
-  
    //Run if location coordinates are valid 
     function success(position) {
         const latitude  = position.coords.latitude;
         const longitude = position.coords.longitude;
-        lat = latitude
-        long = longitude
-        console.log(latitude, longitude)
+        let myTemp = ''
   
         //API url to get local weather
         let openWeatherCall = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=minutely,hourly,daily,alerts' + '&units=imperial' + '&appid=ef63013691934073952193cd8112b3f3'
@@ -22,21 +17,23 @@ function getMyTemp() {
             })
             //Return local temp and add it to the DOM
             .then(function (data){
-                myTemp = data.current.temp
-                var h = document.createElement('h1')
-                h.innerText = myTemp
+                myTemp = data.current.temp.toFixed(0)
+                var h = document.createElement('p')
+                h.id = 'tempText'
+                h.innerText = myTemp + "\xB0F"
                 document.querySelector('#localTemp').appendChild(h)
             })
     }
+
   //Logic for error detection if no location can be returned
     function error() {
-        status.textContent = 'Unable to retrieve your location';
+        textContent = 'Unable to retrieve your location';
     }
   
     if(!navigator.geolocation) {
-        status.textContent = 'Geolocation is not supported by your browser';
+        textContent = 'Geolocation is not supported by your browser';
     } else {
-        status.textContent = 'Locating…';
+        textContent = 'Locating…';
         navigator.geolocation.getCurrentPosition(success, error);
     }
 }
